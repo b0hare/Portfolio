1c9223355359820e4aa5f77fee7ace77c5f48376
Banti Bohare <boharebanti@gmail.com>
2024-04-03 21:42:56 +0530

    Modified Style.css, form, index & script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ let hire_btn = document.getElementById('hire_btn');
 hire_btn.addEventListener("click", function () {
     window.location.href = "form.html";
 })
+
+    // Active button 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+  
+    navLinks.forEach(function(link) {
+      link.addEventListener("click", function() {
+        // Remove 'active' class from all links
+        navLinks.forEach(function(link) {
+          link.classList.remove("clicked");
+        });
+  
+        // Add 'active' class to the clicked link
+        this.classList.add("clicked");
+      });
+    });
+  });
+  
